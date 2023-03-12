24943bd21c389aa993f9cc3d55de91789a46e657
Pablo <percara.pablo@gmail.com>
2023-03-12 17:11:52 -0300

    segunda entrega contador

--- src/componentes/ItemCount/ItemCount.jsx
+++ src/componentes/ItemCount/ItemCount.jsx
@@ -4,6 +4,8 @@ import { useState, useEffect } from 'react'
 
 const ItemCount = ({stockParam}) => {
 
+  const sad = '/public/sad.png'
+
  
   const [contador, setContador] = useState(0)
   const [stockDis, setStock] = useState(stockParam)
@@ -17,17 +19,17 @@ const ItemCount = ({stockParam}) => {
  
   return (
     <div>
-        <p className='stock'>Stock disponible:{stockDis}</p>
+        {stockDis > 0 ? <p className='stock'>Stock disponible: {stockDis}</p> : <p className='s-stock'>Sin Stock <img className='sad' src={sad}></img></p>}
     <div className='contador'>
      
-    <button className='btn' onClick={()=>{
+    <button className='btn-contador' onClick={()=>{
       contador > 0 && setContador(contador - 1)
       contador > 0 && setStock(stockDis + 1)
 
 
     }}>-</button>
     <div>{contador}</div>
-    <button className='btn' onClick={()=>{
+    <button className='btn-contador' onClick={()=>{
       stockDis > 0 && setContador(contador + 1)
       stockDis > 0 && setStock(stockDis - 1)
 
